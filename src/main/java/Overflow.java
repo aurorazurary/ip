b89587f73d8201f9d9b3dc51c6d8f291300e891e
aurorazurary <aurorazurary@gmail.com>
2026-01-23 12:42:29 +0800

    feat: add greetings and farewell

---
+++ src/main/java/Overflow.java
@@ -0,0 +1,9 @@
+public class Overflow {
+    static String GREETINGS = "Good to see you!\nI'm Overflow, lemme know what I could do for you :>";
+    static String FAREWELL = "Looking for the next time we meet!";
+
+    public static void main(String[] args) {
+        System.out.println(GREETINGS);
+        System.out.println(FAREWELL);
+    }
+}
