8dd4d7acdb16aacb9775a1a59de0e5920240d901
aurorazurary <aurorazurary@gmail.com>
2026-01-23 12:49:24 +0800

    feat: exit when user input is 'bye'

--- src/main/java/Overflow.java
+++ src/main/java/Overflow.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Overflow {
     static String GREETINGS = "Good to see you!\nI'm Overflow, lemme know what I could do for you :>";
     static String FAREWELL = "Looking for the next time we meet!";
 
     public static void main(String[] args) {
         System.out.println(GREETINGS);
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            continue;
+        }
         System.out.println(FAREWELL);
     }
 }
