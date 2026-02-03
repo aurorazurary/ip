799834d708b942c5e43662c710d4e24b57d9a320
aurorazurary <aurorazurary@gmail.com>
2026-02-03 17:53:49 +0800

    feature: Add toFileFormat method to Task hierarchy

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -19,6 +19,12 @@ public class Event extends Task {
     }
 
     @Override
+    public String toFileFormat() {
+        return "E | " + super.toFileFormat()
+                + " | " + startTime + " | " + endTime;
+    }
+
+    @Override
     public String toString() {
         return "[E]" + super.toString()
                 + " (from: " + startTime
