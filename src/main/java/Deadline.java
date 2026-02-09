0959c33d25856b5747044c791cbf52977eceae3e
aurorazurary <aurorazurary@gmail.com>
2026-02-09 15:37:39 +0800

    feature: Allow Deadline class to store deadline as LocalDateTime

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,8 +1,11 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  * Represents a task that needs to be done before a specific deadline.
  */
 public class Deadline extends Task {
-    private String deadline;
+    private LocalDateTime deadline;
 
     /**
      * Creates a new deadline task.
@@ -10,19 +13,30 @@ public class Deadline extends Task {
      * @param name Description of the deadline task.
      * @param deadline The deadline by which the task should be completed.
      */
-    public Deadline(String name, String deadline) {
+    public Deadline(String name, LocalDateTime deadline) {
         super(name);
         this.deadline = deadline;
     }
 
-    @Override
-    public String toFileFormat() {
-        return "D | " + super.toFileFormat()
-                + " | " + deadline;
+    /**
+     * Returns the deadline as a LocalDateTime.
+     *
+     * @return The deadline.
+     */
+    public LocalDateTime getDeadline() {
+        return deadline;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + deadline + ")";
+        return "[D]" + super.toString() + " (by: "
+                + deadline.format(DateTimeFormatter.ofPattern("MMM dd yyyy, h:mma")) + ")";
+    }
+
+    @Override
+    public String toFileFormat() {
+        String status = super.toString().contains("X") ? "1" : "0";
+        return "D | " + status + " | " + getName() + " | "
+                + deadline.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
     }
 }
