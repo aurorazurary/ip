382af032985b2e9ac16811722a63641a5d3f3ee0
aurorazurary <aurorazurary@gmail.com>
2026-02-14 20:56:53 +0800

    feat: Creates basic chat layout for GUI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,38 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
  * Main GUI class for Overflow chatbot.
  */
 public class Main extends Application {
+    private VBox dialogContainer;
+    private TextField userInput;
+    private Button sendButton;
+    private ScrollPane scrollPane;
 
     @Override
     public void start(Stage stage) {
-        Label helloWorld = new Label("Hello World from Overflow!"); // Creating a new Label
-        Scene scene = new Scene(helloWorld); // Setting the scene to be our Label
+        // Create components
+        scrollPane = new ScrollPane();
+        dialogContainer = new VBox();
+        scrollPane.setContent(dialogContainer);
 
-        stage.setScene(scene); // Setting the stage to show our scene
-        stage.show(); // Render the stage
+        userInput = new TextField();
+        sendButton = new Button("Send");
+
+        // Layout
+        VBox mainLayout = new VBox(10);
+        mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
+
+        Scene scene = new Scene(mainLayout, 400, 600);
+
+        stage.setTitle("Overflow Chatbot");
+        stage.setScene(scene);
+        stage.show();
     }
 }
